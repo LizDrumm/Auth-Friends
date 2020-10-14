9e4fa3ad73431669bc171207d7eabbd607d0f231
Elizabeth Drumm <elizabethdrumm@Elizabeths-MacBook-Pro.local>
2020-10-14 17:45:28 -0500

    edits

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,77 +1,97 @@
-import React, {useState, useEffect} from "react";
-import { axiosWithAuth } from "./../utils/axiosWithAuth";
+import React, { useEffect, useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-const initialFriendValues={
-    name: '',
-    age: '',
-    email: '',
-}
+const initialFriendValue =
+  // Used to reset and initialize newFriend being typed in
+  {
+    name: "",
+    age: "",
+    email: "",
+  };
 
-const FriendsList = ()=>{
-const [friends, setFriends] = useState([]);
-const [formValues, setFormValues]= useState(initialFriendValues)
+const FriendsList = () => {
+  const [friends, setFriends] = useState([]); // State that will hold all the friends that come in from API
+  const [newFriend, setNewFriends] = useState(initialFriendValue); // State that holds the new friend being adding
 
-// getting the list of friends 
-useEffect(() => {
+  const getData = () => {
+    // We create this function to
+    const token = window.localStorage.getItem("token");
     axiosWithAuth()
-        .get('/friends')
-        .then(res => {
-            // console.log(res.data)
-            setFriends(res.data)
-        })
-        .catch(err => {
-            console.log(err)
-        })
-}, []);
- console.log(friends)
+      .get("./friends/")
+      .then((res) => setFriends(res.data));
+  };
 
+  useEffect(() => {
+    getData();
+  }, []);
 
-
-
-const postData = (newData)=>{
+  const onSubmit = (e) => {
+    e.preventDefault();
     axiosWithAuth()
-    .get('/friends',newData)
-    .then((res)=>{
-    setFriends(friends.concat(res.data))
-    })
-    
-    .catch((err)=> {
-        console.log(err)
-    })
+      .post("./friends/", newFriend)
+      .then((res) => {
+        setFriends(res.data);
+      });
+    setNewFriends(initialFriendValue);
+  };
 
-    .finally(()=>{
-        setFormValues(initialFriendValues)
-    })
-    
-    }
-    
-    const inputChange = (evt)=> {
-        // console.log(evt)
-        setFormValues({
-            ...formValues,
-            [evt.target.name]: evt.target.value
-        });
-    };
+  const onChange = (e) => {
+    setNewFriends({
+      ...newFriend,
+      [e.target.name]: e.target.value,
+    });
+  };
+  return (
+    <div>
+      {friends.map((friend) => {
+        return (
+          <div key={friend.id}>
+            <h1>Name:{friend.name}</h1>
+            <h2>Age:{friend.age}</h2>
+            <h2>E:mail{friend.email}</h2>
+          </div>
+        );
+      })}
 
-    const submit = (evt) => {
-        evt.preventDefault();
-        const newFriend = {
-            id: Date.now(),
-            name: formValues.name.trim(),
-            age: formValues.age.trim(),
-            email: formValues.email.trim(),
-        }
-        postData(newFriend)
-    };
-
-
-return(
-<>
-    {friends.map((friend) => (
-    <h1>return{friend.name}</h1>
-    ))}
-</>
-    )
-    }
+      <div>New Friend adder</div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="name"
+          onChange={onChange}
+          value={newFriend.name}
+          placeholder="name"
+        ></input>
+        <input
+          type="text"
+          name="age"
+          onChange={onChange}
+          value={newFriend.age}
+          placeholder="age"
+        ></input>
+        <input
+          type="text"
+          name="email"
+          onChange={onChange}
+          value={newFriend.email}
+          placeholder="email"
+        ></input>
+        <button onClick={onSubmit}>Add a friend!</button>
+      </form>
+    </div>
+  );
+};
 
 export default FriendsList;
+
+// {
+//   friends.map((friend) => {
+//     return (
+//       <div key={friend.id}>
+//         <h1>Name:{friend.name}</h1>
+//         <h2>Age:{friend.age}</h2>
+//         <h2>E:mail{friend.email}</h2>
+//       </div>
+//     );
+//   });
+// }
